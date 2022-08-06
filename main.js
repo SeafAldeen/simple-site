f673138846af7bc045cfc9c1e2355aefdac09be3
SeafAldeen <95848004+SeafAldeen@users.noreply.github.com>
2022-08-06 17:19:32 +0200

    Create main.js
    Signed-off-by: SeafAldeen <95848004+SeafAldeen@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+/*var menu = document.getElementById("menu");
+
+function closemenu() {
+  menu.style.top = "-200vh" ;
+} 
+
+function openmenu() {
+  menu.style.top = "17%" ;
+} 
+
+*/
+
+var menu = document.getElementById("side-nav");
+
+function closemenu() {
+  menu.style.left = "-200vh" ;
+} 
+
+function openmenu() {
+  menu.style.left = "0%" ;
+} 
+
+
+var body = document.getElementsByName("body");
+ 
